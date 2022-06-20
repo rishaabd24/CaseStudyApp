6d0f6fb4c3001c2dbd663c35501d4706faca6a45
rishaabdhar <rishaabdhar@gmail.com>
2022-06-20 16:18:36 +0530

    Changed the nav bar from default to the CS specifications

---
+++ CaseStudyApp/CaseStudyApp/Pages/UserMgmt.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CaseStudyApp.Pages
+{
+    public class UserMgmtModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
