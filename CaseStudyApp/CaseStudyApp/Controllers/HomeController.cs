72e96088fa70b4d7bd9462cffb67b777e9814fb5
pahal <pahal.duggal@gmail.com>
2022-06-25 18:39:01 +0530

    Added list of files on db to home

--- CaseStudyApp/CaseStudyApp/Controllers/HomeController.cs
+++ CaseStudyApp/CaseStudyApp/Controllers/HomeController.cs
@@ -1,12 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using CaseStudyApp.Models;
+using CaseStudyApp.Model;
+using System.Threading.Tasks;
+using System;
+using Microsoft.EntityFrameworkCore;
+using CaseStudyApp.Data;
+using Microsoft.AspNetCore.Identity;
 
 namespace CaseStudyApp.Controllers
 {
     public class HomeController : Controller
+
+
     {
-        public IActionResult Index()
+        private readonly AuthDbContext context;
+        private readonly UserManager<AppUser> _userManager;
+
+
+        public bool flag = true;
+        public string ErrorMessage { get; set; }
+        public HomeController(AuthDbContext context, UserManager<AppUser> userManager)
         {
-            return View();
+            this.context = context;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var fileuploadViewModel = await LoadAllFiles();
+            ViewBag.userid = _userManager.GetUserName(HttpContext.User);
+            ViewBag.Message = TempData["Message"];
+            return View(fileuploadViewModel);
+        }
+
+        private async Task<FileUploadViewModel> LoadAllFiles()
+        {
+            var viewModel = new FileUploadViewModel();
+            viewModel.FilesOnDatabase = await context.FilesOnDatabase.ToListAsync();
+            viewModel.FilesOnFileSystem = await context.FilesOnFileSystem.ToListAsync();
+            return viewModel;
         }
     }
 }
