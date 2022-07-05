eb9d86a09be53ba41b12a46f9d96a9183ae6a06a
rishaabdhar <rishaabdhar@gmail.com>
2022-07-05 23:46:23 +0530

    Tweaks to UI

--- CaseStudyApp/CaseStudyApp/Models/ChangePasswordViewModel.cs
+++ CaseStudyApp/CaseStudyApp/Models/ChangePasswordViewModel.cs
@@ -17,8 +17,7 @@ namespace CaseStudyApp.Models
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage =
-            "The new password and confirmation password do not match.")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
 }
